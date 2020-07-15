90179fa42f4a314251dbe646628faa708a826e8d
ednezer <ednezerrezende>
2020-07-15 19:32:08 -0300

    improving entryFormComponent

--- src/app/pages/entries/entry-form/entry-form.component.ts
+++ src/app/pages/entries/entry-form/entry-form.component.ts
@@ -6,10 +6,12 @@ import toastr from 'toastr';
 import { Entry } from '../shared/entry.model';
 import { EntryService } from '../shared/entry.service';
 import { switchMap } from 'rxjs/operators';
+import { Category } from '../../categories/shared/category.model';
+import { CategoryService } from '../../categories/shared/category.service';
 
 @Component({
-  selector: 'app-entry-form',
-  templateUrl: './entry-form.component.html',
+  selector: 'app-entry-form' ,
+  templateUrl: './entry-form.component.html' ,
   styleUrls: ['./entry-form.component.css']
 })
 export class EntryFormComponent implements OnInit, AfterContentChecked {
@@ -21,18 +23,60 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
   serverErrorMessages: string[] = null;
   submittingForm = false;
   entry = new Entry();
+  categories: Array<Category>;
 
+  imaskConfig = {
+    mask: Number,
+    scale: 2,
+    thousandsSeparator: '' ,
+    padFractionalZeros: true,
+    normalizeZeros: true,
+    radix: ' , '
+  };
+
+  ptBR = {
+    closeText: 'Fechar' ,
+    prevText: 'Anterior' ,
+    nextText: 'Próximo' ,
+    // currentText: 'Começo' ,
+    monthNames: ['Janeiro' , ' Fevereiro' , ' Março' , ' Abril' , ' Maio' , ' Junho' ,
+    'Julho' , ' Agosto' , ' Setembro' , ' Outubro' , ' Novembro' , ' Dezembro'],
+    monthNamesShort: ['Jan' , ' Fev' , ' Mar' , ' Abr' , ' Mai' , ' Jun' , 'Jul' , ' Ago' , ' Set' , ' Out' , ' Nov' , ' Dez'],
+    dayNames: ['Domingo' , ' Segunda' , ' Terça' , ' Quarta' , ' Quinta' , ' Sexta' , ' Sábado'],
+    dayNamesShort: ['Dom' , ' Seg' , ' Ter' , ' Qua' , ' Qui' , ' Sex' , ' Sáb'],
+    dayNamesMin: ['D' , ' S' , ' T' , ' Q' , ' Q' , ' S' , ' S'],
+    weekHeader: 'Semana' ,
+    firstDay: 1,
+    isRTL: false,
+    today: 'Hoje',
+    clear: 'Limpar',
+    showMonthAfterYear: false,
+    yearSuffix: '' ,
+    timeOnlyTitle: 'Só Horas' ,
+    timeText: 'Tempo' ,
+    hourText: 'Hora' ,
+    minuteText: 'Minuto' ,
+    secondText: 'Segundo' ,
+    currentText: 'Data Atual',
+    ampm: false,
+    month: 'Mês' ,
+    week: 'Semana' ,
+    day: 'Dia' ,
+    allDayText : 'Todo Dia'
+  }
   constructor(
     private entryService: EntryService,
     private route: ActivatedRoute,
     private router: Router,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private categorieService: CategoryService
   ) { }
 
   ngOnInit(): void {
     this.setCurrentAction();
     this.buildEntryForm();
     this.loadEntry();
+    this.loadCategories();
   }
 
   ngAfterContentChecked() {
@@ -48,6 +92,16 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
     }
   }
 
+  get typeOptions(): Array<any> {
+    return Object.entries(Entry.types).map(
+      ([value, text]) => {
+        return {
+          text,
+          value
+        };
+      }
+    );
+  }
   createEntry(){
     const entry = Object.assign(new Entry(), this.entryForm.value);
     this.entryService.create(entry).subscribe(
@@ -58,8 +112,8 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
 
   private actionsForSuccess(entry: Entry){
     toastr.success('Solicitação processada com sucesso!');
-    this.router.navigateByUrl('entries', {skipLocationChange: true}).then(
-      () => this.router.navigate(['entries', entry.id, 'edit'])
+    this.router.navigateByUrl('entries' , {skipLocationChange: true}).then(
+      () => this.router.navigate(['entries' , entry.id, 'edit'])
     );
   }
 
@@ -104,10 +158,10 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
       id: [null],
       name: [null, [Validators.required, Validators.minLength(2)]],
       description: [null],
-      type: [null, [Validators.required]],
+      type: ['expense', [Validators.required]],
       amount: [null, [Validators.required]],
       date: [null, [Validators.required]],
-      paid: [null, [Validators.required]],
+      paid: [true, [Validators.required]],
       categoryId: [null, [Validators.required]]
     });
   }
@@ -123,6 +177,12 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
         }, error => alert('Ocorreu um erro no servidor, tente mais tarde')
       );
     }
+  }
+
+  loadCategories(){
+    this.categorieService.getAll().subscribe(
+      resposta => this.categories = resposta
+    );
   }
 
 }
